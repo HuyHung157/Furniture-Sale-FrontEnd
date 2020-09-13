23602a477f8baa5f24a807e54dbf1ccc79fe452f
HuyHung157 <hhungtran157@gmail.com>
2020-09-13 21:29:24 +0700

    commit basic header with responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,20 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
     loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./pages/about/about.module').then((m) => m.AboutModule),
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./pages/contact/contact.module').then((m) => m.ContactModule),
   }
 ];
 
---
+++ src/app/pages/contact/contact.component.html
@@ -0,0 +1,1 @@
+<h1>page Contact</h1>
